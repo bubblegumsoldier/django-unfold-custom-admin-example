c35e24acead7dad7dcdf8172504cb1cacf944128
Henry Müssemann <hm@maltego.com>
2024-08-07 15:19:25 +0200

    reproduced weird looking buttons

--- custom_admin/admin.py
+++ custom_admin/admin.py
@@ -1,19 +1,31 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from unfold import admin as unfold_admin
+from unfold.admin import ModelAdmin
+from unfold.sites import UnfoldAdminSite
 
+from custom_admin.models import CustomGroup, CustomGroupToUser
 
 # Register your models here.
-class CustomAdminSite(admin.AdminSite):
+class CustomAdminSite(UnfoldAdminSite):
     def get_urls(self):
         urls = super(CustomAdminSite, self).get_urls()
         custom_urls = []
         return custom_urls + urls
 
+custom_admin_site = CustomAdminSite(name="snh2_dj_admin")
 
-class UserAdmin(unfold_admin.ModelAdmin):
+@admin.register(User, site=custom_admin_site)
+class UserAdmin(ModelAdmin):
+    search_fields = ['username', 'email']
     model = User
 
+class CustomGroupToUserInline(admin.TabularInline):
+    model = CustomGroupToUser
+    autocomplete_fields = ['user', 'group']
+    tab = True
 
-custom_admin_site = CustomAdminSite(name="snh2_dj_admin")
-custom_admin_site.register(User, UserAdmin)
+@admin.register(CustomGroup, site=custom_admin_site)
+class CustomGroupAdmin(ModelAdmin):
+    model = CustomGroup
+    search_fields = ['name']
+    inlines = [CustomGroupToUserInline]
--- custom_admin/models.py
+++ custom_admin/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class CustomGroup(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
+class CustomGroupToUser(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    group = models.ForeignKey(CustomGroup, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.user} - {self.group}"
